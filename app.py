7320bc978529d917470a7f5433de328cbf9daef7
Aaron Herman <aaron@aaronherman.co>
2017-10-24 12:21:20 -0500

    decode

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import urllib.request
+import json
 
 with urllib.request.urlopen("http://www.projectdoama.com/api/v1/getMovieInfo/Harry") as response:
     html = response.read()
 
-print(html)
+output = html.decode('utf-8')
 
+obj = json.loads(output)
+
+for item in obj:
+    print(obj)
